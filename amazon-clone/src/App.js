b5a443e3dd8e09c85b3dd73225b88444419ccbd8
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2021-01-07 15:49:32 +0000

    updated the dispatch and reducer

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Home from './Home';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import CheckOut from './CheckOut'
 
 import './App.css';
 
@@ -11,7 +12,7 @@ function App() {
       <div className="app">
         <Switch>
           <Route path="/checkout">
-            <h1>Checkout </h1>
+            <CheckOut />
           </Route>
 
           <Route path="/login">
---
+++ amazon-clone/src/CheckOut.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CheckOut() {
+    return (
+        <div className="checkout">
+            <h1>This is the Checkout</h1>
+        </div>
+    )
+}
+
+export default CheckOut
